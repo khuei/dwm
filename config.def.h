b1057b1ca0722fd8bbcccebbae0622cf761f099a
Khue Nguyen <Z5483Y@gmail.com>
2021-11-13 17:19:48 -0500

    Remove monocle layout

--- config.def.h
+++ config.def.h
@@ -49,7 +49,6 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
 };
 
 /* key definitions */
